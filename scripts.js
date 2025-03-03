ef5c795f223ef84ba5a39c6445a98d8c4f83a428
IrateLizard20 <jbbusiness2007@gmail.com>
2025-03-03 00:56:19 -0500

    upload

---
+++ scripts.js
@@ -0,0 +1,36 @@
+function butterfly() {
+    document.getElementById("img").style = "background-image: url(./imgs/profiles/butterfly.png)";
+    document.getElementById("txt").innerHTML = "backstory stuff goes here 1";
+}
+function cassette() {
+    document.getElementById("img").style = "background-image: url(./imgs/profiles/placeholder.png)";
+    document.getElementById("txt").innerHTML = "backstory stuff goes here 2";
+}
+function flintlock() {
+    document.getElementById("img").style = "background-image: url(./imgs/profiles/placeholder.png)";
+    document.getElementById("txt").innerHTML = "backstory stuff goes here 3";
+}
+function fork() {
+    document.getElementById("img").style = "background-image: url(./imgs/profiles/placeholder.png)";
+    document.getElementById("txt").innerHTML = "backstory stuff goes here 4";
+}
+function hockeypuck() {
+    document.getElementById("img").style = "background-image: url(./imgs/profiles/placeholder.png)";
+    document.getElementById("txt").innerHTML = "backstory stuff goes here 5";
+}
+function lighter() {
+    document.getElementById("img").style = "background-image: url(./imgs/profiles/placeholder.png)";
+    document.getElementById("txt").innerHTML = "backstory stuff goes here 6";
+}
+function scissors() {
+    document.getElementById("img").style = "background-image: url(./imgs/profiles/placeholder.png)";
+    document.getElementById("txt").innerHTML = "backstory stuff goes here 7";
+}
+function thewind() {
+    document.getElementById("img").style = "background-image: url(./imgs/profiles/placeholder.png)";
+    document.getElementById("txt").innerHTML = "backstory stuff goes here 8";
+}
+function washer() {
+    document.getElementById("img").style = "background-image: url(./imgs/profiles/placeholder.png)";
+    document.getElementById("txt").innerHTML = "backstory stuff goes here 9";
+}
